9919c5627ba2c85139b9dbf12174c22cebf5770b
Ayo-Oluwa Adebayo <ayo-oluwa.adebayo@andela.com>
2019-09-23 12:33:53 +0100

    Add quiz page

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
-  <div id="app">
-    
+  <div id="app" class="container">
+    <QuizPage/>
   </div>
 </template>
 
 <script>
+import QuizPage from './components/QuizPage'
 export default {
-
+  components: {
+    QuizPage
+  }
 }
 </script>
 
---
+++ src/components/QuizPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <h1 class="text-center">The Super Quiz</h1>
+      </div>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+        <component :is="mode" @answered="answered($event)" @confirmed="mode = 'Question'"></component>
+      </div>
+    </div>   
+  </div>
+</template>
+
+<script>
+import Question from './Question.vue'
+import Answer from './Answer.vue'
+export default {
+  components: {
+    Question,
+    Answer
+  },
+  data () {
+    return {
+      mode: 'Question'
+    }
+  },
+  methods: {
+    answered (isCorrect) {
+      if (isCorrect) {
+        this.mode = 'Answer'
+      } else {
+        this.mode = 'Question'
+        alert('Wrong, try again!')
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
